53b5df9ca8ea0f7e5ccb291908517ff70d215c13
anandgarg91 <anandgarg91@gmail.com>
2018-12-06 16:36:59 +0800

    slowhttp

---
+++ src1/attack.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re
+import subprocess
+import os
+username=''
+password=''
+passfile  = open("test.txt", "r")
+ip = ''
+flag = 1
+for line in passfile:
+    if re.match(".*Nmap scan.*", line) and flag:
+            ip  = line.split(' ')[4]
+            flag = 0
+        
+    if re.match(".*Username:.*", line):
+        username =  line.split(' ')[2]
+#        print username
+    if re.match(".*Password:.*", line):
+        password =  line.split(' ')[2]
+#        print password
+  
+print username+" "+password+" "+ip
+   # os.system("ssh"+" "+username+"@172.16.10.2")
+
